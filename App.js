c072406e2ca8df3840b394826579e3ee42e384d9
Clement Guillot <clementguillot@MacBook-Pro-de-Clement.local>
2022-08-02 16:49:28 +0200

    clément modifs

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ const BottomNavigator = () => {
 export default function App() {
   return (
 
-    <HomeScreen/>
+    <SunshineScreen/>
 
       // <NavigationContainer>
       //   <Stack.Navigator screenOptions={{headerShown: false}}>
