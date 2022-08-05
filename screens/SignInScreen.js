f5032b643fef05cceba28aa4772cedd18056df4c
mimicalzelunghe <mimicalzelunghe@gmail.com>
2022-08-05 15:23:16 +0200

    mimic homescreen

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -27,7 +27,6 @@ function SignInScreen(props){
     // then show climateScreen else show HomeScreen
     // isUnknownUser value depends on the login process result
     var screenToDisplay;
-
     
     var handleSubmitConnectionRequest = async ()=>{
 
@@ -35,10 +34,6 @@ function SignInScreen(props){
       var hasGarden = false
 
       // upload user
-      console.log("Mimic2: SignInScreen - dans user() avant le fetch")
-
-      console.log("Mimic14: SignInScreen - dans user() ipadress :", backendIpAdress+'/users/signIn' )
-
       const userData = await fetch(backendIpAdress+'/users/signIn', {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
@@ -47,53 +42,33 @@ function SignInScreen(props){
 
       //retranscription de la réponse pour qu'on puisse la lire
       const userBody = await userData.json()
-      console.log("Mimic1:  SignInScreen - fetch terminé - userBody:", userBody)
 
       if(userBody.result == true){
-        //console.log("Mimic7:  SignInScreen - result body est true:", userBody)
-        knownUser == true
-        console.log("Mimic7:  SignInScreen - knownUser ? ", knownUser)
-        //setIsUnknownUser(false)
+        console.log("Mimic7:  SignInScreen - result body est true:", userBody)
+        knownUser = true
         setToken(userBody.token)
         props.receivedToken(userBody.token)
-        console.log("Mimic10:  SignInScreen - valeur de isUnknownUser:", isUnknownUser)
 
         if(userBody.userGardens.length != 0){
-          //console.log("Mimic8:  SignInScreen - il n'y a pas de garden:", hasNoGarden)
-          //setHasNoGarden(false)
-          hasGarden == true
-          console.log("Mimic8:  SignInScreen - il n'y a pas de garden:", hasGarden)
+          hasGarden = true
+          console.log("Mimic8:  SignInScreen - hasGarden is true:", hasGarden)
         }
         
       }  else {
-        console.log("Mimic9:  SignInScreen - user est inconnu:", userBody.result)
-        //setIsUnknownUser(true)
-        knownUser == false
+        knownUser = false
         setEmailError(userBody.error)
         setPasswordError(userBody.errorr)
       }
   
   
-
-      console.log("Mimic3: choix ecran à afficher")
-      console.log("Mimic5:  SignInScreen - test affichage prochaine page, isUnknownUser, hasNoGarden ", isUnknownUser, hasNoGarden )
       // call to backend: is the user known?
       // if known user and if she has a garden the display the home
-      //if(isUnknownUser){
-      if(knownUser){
-        //screenToDisplay = <Button title="Go page to SignUp screen" onPress={() => props.navigation.navigate('SignUp')} />
-        
+      if(!knownUser){
         props.navigation.navigate("SignUp")
-        console.log("Mimic11:  SignInScreen - j'active signUp")
-      //}else if(hasNoGarden){// else, go to HomeScreen and show the gardens
-      }else if(hasGarden){// else, go to HomeScreen and show the gardens
-        //screenToDisplay = <Button title="Go page to Welcome screen" onPress={() => props.navigation.navigate('Welcome')} />
+      }else if(!hasGarden){// else, go to HomeScreen and show the gardens
         props.navigation.navigate("Welcome")
-        console.log("Mimic12:  SignInScreen - j'active welcome")
       }else{// else, display the garden of this well known user
-        //screenToDisplay = <Button title="Go page to HomeScreen" onPress={() => props.navigation.navigate('Home')} />
         props.navigation.navigate("Home")
-        console.log("Mimic13:  SignInScreen - j'active Home")
       }
 
     }
